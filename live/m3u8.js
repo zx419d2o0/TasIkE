2862536de8a4e6fb79fef57932a0735c4f2ae223
zx419d2o0 <869969681@qq.com>
2024-04-17 17:30:10 +0800

    Update m3u8.js

--- live/m3u8.js
+++ live/m3u8.js
@@ -5,8 +5,8 @@ const fs = require('fs')
 const urls = [
   'https://hub.gitmirror.com/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u',
   // 'https://hub.gitmirror.com/https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u',
-  'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u',
-  'https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/newbestv.m3u',
+  'https://hub.gitmirror.com/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/bestv.m3u',
+  'https://hub.gitmirror.com/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/newbestv.m3u',
   'https://live.fanmingming.com/tv/m3u/ipv6.m3u',
   'https://hub.gitmirror.com/https://raw.githubusercontent.com/Slive8/iTV/main/Slive.m3u'
 ]
