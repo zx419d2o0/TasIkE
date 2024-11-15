70da642f6a212063ab05cc1516e2dd834bddd2bf
zx419d2o0 <869969681@qq.com>
2024-11-15 13:58:42 +0800

    Update merge.js

--- tv/live/merge.js
+++ tv/live/merge.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 const m3us = [
   'https://tv.iill.top//m3u/Gather',
+  'https://bit.ly/IPTV-Gather',
   'https://live.fanmingming.com/tv/m3u/ipv6.m3u',
   'https://hub.gitmirror.com/https://raw.githubusercontent.com/Slive8/iTV/main/Slive.m3u',
   'https://github.moeyy.xyz/https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u',
